c7d60a7abadb60af3a64bcb34eb802539f68b9bf
nipdep <nipun1deelaka@gmail.com>
2021-02-26 00:43:59 +0530

    Add: folder

---
+++ qulification round/map.py
@@ -0,0 +1,16 @@
+class Map(object):
+    def __init__(self, second):
+        self.second = second
+        pass
+
+    def car_states():
+        pass
+
+    def traffic_light_state():
+        pass
+
+    def road_queues():
+        pass
+
+    
+
---
+++ qulification round/simulation.py
@@ -0,0 +1,3 @@
+
+def sim():
+    pass
